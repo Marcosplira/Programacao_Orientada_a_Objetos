91f4cb0778b6487ce06eef99bade5e09b5f58390
Marcosplira <61232814+Marcosplira@users.noreply.github.com>
2024-07-11 13:24:28 +0000

    atividade 01

---
+++ Aulas01-20-02-2024-21-02-2024/Carro.java
@@ -0,0 +1,18 @@
+public class Carro{
+    String marca; 
+    String modelo; 
+    int ano = 2017; 
+    double km; 
+    String cor;
+    double velocidade = 0; 
+  
+    double acelerar(){
+      velocidade = velocidade + 1;
+      return velocidade;
+    }
+    
+    double andar(double KmPercorrido){
+      km = km + KmPercorrido;
+      return KmPercorrido;  
+    }
+  }
---
+++ Aulas01-20-02-2024-21-02-2024/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+      Carro meuCarro = new Carro();
+      System.out.println(meuCarro.ano);
+      System.out.println("Ano do Carro: " + meuCarro.ano);
+      System.out.println("Velocidade do Carro: " + meuCarro.velocidade);
+      System.out.println("Km do Carro: " + meuCarro.km);
+      meuCarro.andar(10.0);
+      // System.out.println("Cor do Carro: " + meuCarro.cor);
+      // System.out.println("Marca do Carro: " + meuCarro.marca);
+      // System.out.println("Modelo do Carro: " + meuCarro.modelo);
+      System.out.println("Km do Carro: " + meuCarro.km);
+      meuCarro.andar(20.5); 
+      System.out.println("Km do Carro: " + meuCarro.km);
+    }
+  }
